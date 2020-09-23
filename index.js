3f08f58325c348e5fbbb852aee593ed4f1178e36
tpope0928 <tpope0928@gmail.com>
2020-09-22 21:01:53 -0400

    console.log array

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     alert('LOADED');
+    const endPoint = 'http://localhost:3000/api/v1/players'
+
+    fetch(endPoint)
+        .then(res => res.json())
+        .then(json => console.log(json));
 });
 
