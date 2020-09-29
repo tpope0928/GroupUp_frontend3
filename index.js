a93c60d4cc63750b5c5a048e9b8881d3e46481bf
tpope0928 <tpope0928@gmail.com>
2020-09-28 21:55:21 -0400

    reset

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const endPoint = 'http://localhost:3000/api/v1/players'
 
-    fetch(endPoint)
-        .then(res => res.json())
-        .then(json =>
-            json.forEach(player => {
-                const playerMarkup = `
-                <h3>${player.name} - ${player.city}, ${player.state}
-                </h3>`;
-
-                document.querySelector('#player-game-list').innerHTML += playerMarkup;
-            })
-        );
-});
 
 
