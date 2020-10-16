368270402690e0d4d486b4d5560eace3ed61152e
tpope0928 <tpope0928@gmail.com>
2020-10-16 18:37:18 -0400

    snap back to reality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+const BACKEND_URL = "http://localhost:3000";
+const PLAYERS_URL = `${BACKEND_URL}/api/v1/players`;
+const GAMES_URL = `${BACKEND_URL}/api/v1/games`;
 
+document.addEventListener("DOMContentLoaded", function () {
+    Player.allInstances = [];
+    Player.fetchPlayers();
+    Game.allInstances = [];
+    Game.fetchGames();
+
+    let playerButton = document.querySelector("#player_index");
+    playerButton.addEventListener("click", Player.showPlayers);
+
+    let gameButton = document.querySelector("#games_index");
+    gameButton.addEventListener("click", Game.showGames);
+
+    let createPlayerButton = document.querySelector("#create_player");
+    createPlayerButton.addEventListener("click", Player.showPlayerForm);
+
+    let createGameButton = document.querySelector("#create_games");
+    createGameButton.addEventListener("click", Game.showGameForm);
+})
