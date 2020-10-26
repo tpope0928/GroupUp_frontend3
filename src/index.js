a34fa0bff76662baaf28b51c2e529f159b5763b8
tpope0928 <tpope0928@gmail.com>
2020-10-25 20:49:55 -0400

    yay got it to work

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ const BACKEND_URL = "http://localhost:3000";
 const PLAYERS_URL = `${BACKEND_URL}/api/v1/players`;
 const GAMES_URL = `${BACKEND_URL}/api/v1/games`;
 
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(PLAYERS_URL)
+        .then(res => res.json())
+        .then(json => console.log(json))
+});
