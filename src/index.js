88db1f4fb50fee70d1b4f442d781f3ddf908e97b
tpope0928 <tpope0928@gmail.com>
2020-10-25 22:09:15 -0400

    EOD 10/25/20

--- src/index.js
+++ src/index.js
@@ -3,25 +3,12 @@ const PLAYERS_URL = `${BACKEND_URL}/api/v1/players`;
 const GAMES_URL = `${BACKEND_URL}/api/v1/games`;
 
 document.addEventListener('DOMContentLoaded', () => {
-    getPlayers();
-});
-
-function getPlayers() {
     fetch(PLAYERS_URL)
         .then(res => res.json())
         .then(players => {
             players.data.forEach(player => {
-                const playerMarkup = `
-                    <div data-id=${player.id}>
-                        <h3>${player.attributes.name} - ${player.attributes.city}, ${player.attributes.state}</h3>
-                        <p>Game: ${player.attributes.games.title}</p>
-                        <p>Genre: ${player.attributes.games.genre}</p>
-                        <p>Game Level: ${player.attributes.games.skill_level}</p>
-                        <button data-id=${player.id}>Group Up!</button>
-                    </div>
-                <br><br>`;
-
-                document.querySelector('#content-container').innerHTML += playerMarkup
-            })
-        })
-}
+                const newPlayer = new Player(player);
+                document.querySelector('#content-container').innerHTML += newPlayer.renderListItem();
+            });
+        });
+});
--- src/player.js
+++ src/player.js
@@ -1,10 +1,19 @@
 class Player {
-    //Params associated with a Player
-    constructor(player, playerAttr) {
+    constructor(player) {
         this.id = player.id;
-        this.name = playerAttr.name;
-        this.city = playerAttr.city;
-        this.state = playerAttr.state;
-        this.games = playerAttr.games;
+        this.name = player.name;
+        this.city = player.city;
+        this.state = player.state;
+        Player.all.push(this);
+        console.log(this);
+    }
+
+    renderListItem() {
+        return `
+        <h3>${this.name} - ${this.city}, ${this.state}
+        </h3>`
     }
 }
+
+
+Player.all = [];
