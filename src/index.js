040c1084f6a3301d67601a4ff893e021528a48aa
tpope0928 <tpope0928@gmail.com>
2020-10-25 21:11:32 -0400

    player data shows up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ const PLAYERS_URL = `${BACKEND_URL}/api/v1/players`;
 const GAMES_URL = `${BACKEND_URL}/api/v1/games`;
 
 document.addEventListener('DOMContentLoaded', () => {
+    getPlayers();
+});
+
+function getPlayers() {
     fetch(PLAYERS_URL)
         .then(res => res.json())
-        .then(json => console.log(json))
-});
+        .then(players => {
+            players.data.forEach(player => {
+                const playerMarkup = `
+                <h3>${player.attributes.name} - ${player.attributes.city}, ${player.attributes.state}</h3>
+                `;
+
+                document.querySelector('#content-container').innerHTML += playerMarkup
+            })
+        })
+}
