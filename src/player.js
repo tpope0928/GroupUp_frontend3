681bd3771151f42788c0dc7ab19eb0ca57fbb5a6
tpope0928 <tpope0928@gmail.com>
2020-11-15 21:58:10 -0500

    EOD 11/15/2020

--- src/player.js
+++ src/player.js
@@ -1,10 +1,13 @@
 class Player {
     constructor(player, playerAtt) {
         this.id = player.id
+        //debugger
         this.name = playerAtt.name
         this.city = playerAtt.city
         this.state = playerAtt.state
         this.games = playerAtt.games
+
+        //this.title = playerAtt.games.title
 
 
         Player.all.push(this)
@@ -12,12 +15,15 @@ class Player {
     }
 
     renderPlayerCard() {
+        //debugger
         return `
-            <h2>${this.name} - ${this.city}, ${this.state}</h2>
-            <h4>${this.games.title}</h4>
-            <p>${this.games.genre}</p>
-            <p>${this.games.skill_level}</p>
-            <p>${this.games.game_name}</p>
+            <div data-id="${this.id}">
+                <h2>${this.name} - ${this.city}, ${this.state}</h2>
+                <h4>${this.games.title}</h4>
+                <p>${this.games.genre}</p>
+                <p>${this.games.skill_level}</p>
+                <p>${this.games.game_name}</p>
+            </div>
         `
 
 
